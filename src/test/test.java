457eb21acf985f1cd99f7c7d0a3ca77a26d5cbd4
huangshaojun <huangshaojun@sensorsdata.cn>
2023-03-30 11:00:15 +0800

    Initial commit

---
+++ src/test/test.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * TODO
+ *
+ * @author huangshaojun
+ * @version 1.0.0
+ * @since 2023/03/30 10:59
+ */
+public class test {
+  public static void main(String[] args) {
+    String text = "this is 1 ";
+  }
+}
